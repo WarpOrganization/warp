83e92a718ad31ac5b55ef2464e65c320e6265ddc
Jaca777 <szymonrodant@gmail.com>
2016-06-28 17:03:26 +0200

    OBJ model loader & PNG decoding

---
+++ graphics/src/main/java/pl/warp/engine/graphics/resource/mesh/ObjLoader.java
@@ -0,0 +1,201 @@
+package pl.warp.engine.graphics.resource.mesh;
+
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import pl.warp.engine.graphics.mesh.Mesh;
+import pl.warp.engine.graphics.mesh.VAOMesh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jaca777
+ *         Created 22.12.14 at 15:22
+ */
+public class ObjLoader {
+
+    private static class Vertex {
+        Vector3f pos;
+        Vector2f texCoord;
+        Vector3f normal;
+        private int normalWeight = 1;
+
+        private Vertex(Vector3f pos, Vector2f texCoord, Vector3f normal) {
+            this.pos = pos;
+            this.texCoord = texCoord;
+            this.normal = normal;
+        }
+
+        /**
+         * @param texCoord Texture coordinate of vertex being mixed.
+         * @param normal   Normal of vertex being mixed.
+         * @return whether the mixture is possible (both have the same texCoords)
+         */
+        private boolean mix(Vector2f texCoord, Vector3f normal) {
+            if (this.texCoord.x != texCoord.x || this.texCoord.y != texCoord.y) {
+                return false;
+            } else {
+                if (this.normal.x != normal.x || this.normal.y != normal.y || this.normal.z != normal.z) {
+                    this.normal.mul(normalWeight / (float) (normalWeight + 1));
+                    this.normal.div(normalWeight + 1);
+                    this.normal.add(normal);
+                    this.normal.normalize();
+                }
+                return true;
+            }
+        }
+    }
+
+    private Vertex[] vertices;
+    private List<Vertex> hVertices;
+    private List<Integer> indices;
+
+    private static final String VERTEX = "v";
+    private static final String TEXTURE = "vt";
+    private static final String NORMAL = "vn";
+    private static final String FACE = "f";
+
+    public static ObjLoader read(InputStream objFile) {
+        ObjLoader obj = new ObjLoader();
+        List<Vector3f> vertices = new ArrayList<>();
+        List<Vector3f> normals = new ArrayList<>();
+        List<Vector2f> texCoords = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(objFile));
+
+        try {
+            String line = null;
+            boolean facesReached = false;
+            while (!facesReached && (line = reader.readLine()) != null) {
+                String[] data = line.split(" ");
+                switch (data[0]) {
+                    case VERTEX:
+                        vertices.add(new Vector3f(Float.parseFloat(data[1]), Float.parseFloat(data[2]), Float.parseFloat(data[3])));
+                        break;
+                    case TEXTURE:
+                        texCoords.add(new Vector2f(Float.parseFloat(data[1]), Float.parseFloat(data[2])));
+                        break;
+                    case NORMAL:
+                        normals.add(new Vector3f(Float.parseFloat(data[1]), Float.parseFloat(data[2]), Float.parseFloat(data[3])));
+                        break;
+                    case FACE:
+                        facesReached = true;
+                        break;
+                }
+            }
+
+            obj.setupArrays(vertices.size());
+
+            while (line != null) {
+                if (!line.startsWith("f")) {
+                    line = reader.readLine();
+                    continue;
+                }
+                String[] faceData = line.split(" ");
+                obj.processVertex(faceData[1].split("/"), vertices, normals, texCoords);
+                obj.processVertex(faceData[2].split("/"), vertices, normals, texCoords);
+                obj.processVertex(faceData[3].split("/"), vertices, normals, texCoords);
+                line = reader.readLine();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return obj;
+    }
+
+    private void setupArrays(int size) {
+        this.hVertices = new ArrayList<>();
+        this.vertices = new Vertex[size];
+        this.indices = new ArrayList<>(size);
+    }
+
+
+    public void processVertex(String[] vertexData, List<Vector3f> verticesList, List<Vector3f> normalsList, List<Vector2f> texCoordsList) {
+        int pointer = Integer.parseInt(vertexData[0]) - 1;
+        Vector3f pos = verticesList.get(pointer);
+        Vector2f texCoord = texCoordsList.get(Integer.parseInt(vertexData[1]) - 1);
+        Vector3f normal = normalsList.get(Integer.parseInt(vertexData[2]) - 1);
+        if (this.vertices[pointer] != null) {
+            Vertex v = this.vertices[pointer];
+            if (v.mix(texCoord, normal)) {
+                this.indices.add(pointer);
+            } else {
+                Vertex vertex = new Vertex(pos, texCoord, normal);
+                this.hVertices.add(vertex);
+                this.indices.add(this.vertices.length + hVertices.size() - 1);
+            }
+        } else {
+            this.vertices[pointer] = new Vertex(pos, texCoord, normal);
+            this.indices.add(pointer);
+        }
+    }
+
+    public Mesh toMesh(int[] attributes) {
+        int size = this.vertices.length + hVertices.size();
+        float[] v = new float[size * 4];
+        float[] t = new float[size * 2];
+        float[] n = new float[size * 3];
+        int[] indices = new int[this.indices.size()];
+        load(v, t, n, indices);
+        return new Mesh(v, t, n, indices, attributes);
+    }
+
+    public VAOMesh toVAOMesh(int[] attributes) {
+        int size = this.vertices.length + hVertices.size();
+        float[] v = new float[size * 4];
+        float[] t = new float[size * 2];
+        float[] n = new float[size * 3];
+        int[] indices = new int[this.indices.size()];
+        load(v, t, n, indices);
+        return new VAOMesh(v, t, n, indices, attributes);
+    }
+
+    private void load(float[] v, float[] t, float[] n, int[] indices) {
+        int i = 0;
+        for (; i < this.vertices.length; i++)
+            if (this.vertices[i] != null) {
+                Vector3f pos = this.vertices[i].pos;
+                Vector2f texCoord = this.vertices[i].texCoord;
+                Vector3f normal = this.vertices[i].normal;
+                v[i * 4] = pos.x;
+                v[i * 4 + 1] = pos.y;
+                v[i * 4 + 2] = pos.z;
+                v[i * 4 + 3] = 1f;
+                t[i * 2] = texCoord.x;
+                t[i * 2 + 1] = 1 - texCoord.y;
+                n[i * 3] = normal.x;
+                n[i * 3 + 1] = normal.y;
+                n[i * 3 + 2] = normal.z;
+            }
+
+        for (Vertex vertex : this.hVertices) {
+            Vector3f pos = vertex.pos;
+            Vector2f texCoord = vertex.texCoord;
+            Vector3f normal = vertex.normal;
+            v[i * 4] = pos.x;
+            v[i * 4 + 1] = pos.y;
+            v[i * 4 + 2] = pos.z;
+            v[i * 4 + 3] = 1f;
+            t[i * 2] = texCoord.x;
+            t[i * 2 + 1] = 1 - texCoord.y;
+            n[i * 3] = normal.x;
+            n[i * 3 + 1] = normal.y;
+            n[i * 3 + 2] = normal.z;
+            i++;
+        }
+        for (i = 0; i < this.indices.size(); i++) {
+            indices[i] = this.indices.get(i);
+        }
+    }
+}
---
+++ graphics/src/main/java/pl/warp/engine/graphics/resource/texture/ImageDecoder.java
@@ -0,0 +1,116 @@
+package pl.warp.engine.graphics.resource.texture;
+
+import org.lwjgl.BufferUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+/**
+ * @author Jaca777
+ *         Created 25.12.14 at 20:23
+ */
+public class ImageDecoder {
+    /**
+     * @param src Encoded PNG stream
+     * @return Dimension of images (width, height)
+     */
+    public static DecodedImage decodePNG(InputStream src, PNGDecoder.Format format) {
+        ByteBuffer buffer = null;
+        int w = 0, h = 0;
+        try {
+            PNGDecoder decoder = new PNGDecoder(src);
+            buffer = BufferUtils.createByteBuffer(format.getNumComponents() * decoder.getWidth() * decoder.getHeight());
+            decoder.decode(buffer, decoder.getWidth() * format.getNumComponents(), format);
+            w = decoder.getWidth();
+            h = decoder.getHeight();
+            buffer.flip();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                src.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new DecodedImage(buffer, w, h);
+    }
+
+
+    /**
+     * All encoded images must be the same size.
+     */
+    public static DecodedImagesArray decodePNGs(InputStream[] src, PNGDecoder.Format format) {
+        ByteBuffer[] buffers = new ByteBuffer[src.length];
+        int w = 0, h = 0;
+        for (int i = 0; i < src.length; i++) {
+            try {
+                PNGDecoder decoder = new PNGDecoder(src[i]);
+                buffers[i] = BufferUtils.createByteBuffer(format.getNumComponents() * decoder.getWidth() * decoder.getHeight());
+                decoder.decode(buffers[i], decoder.getWidth() * format.getNumComponents(), format);
+                w = decoder.getWidth();
+                h = decoder.getHeight();
+                buffers[i].flip();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    src[i].close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return new DecodedImagesArray(buffers, w, h);
+    }
+
+    public static final class DecodedImage {
+        private ByteBuffer buffer;
+        private int w, h;
+
+        public DecodedImage(ByteBuffer buffer, int w, int h) {
+            this.buffer = buffer;
+            this.w = w;
+            this.h = h;
+        }
+
+        public ByteBuffer getBuffer() {
+            return buffer;
+        }
+
+        public int getW() {
+            return w;
+        }
+
+        public int getH() {
+            return h;
+        }
+
+    }
+
+    public static final class DecodedImagesArray {
+        private ByteBuffer[] buffers;
+        private int w, h;
+
+        public DecodedImagesArray(ByteBuffer[] buffers, int h, int w) {
+            this.buffers = buffers;
+            this.w = w;
+            this.h = h;
+        }
+
+        public ByteBuffer[] getBuffers() {
+            return buffers;
+        }
+
+        public int getW() {
+            return w;
+        }
+
+        public int getH() {
+            return h;
+        }
+    }
+
+}
