f63554a81e436b1440dcc48bdb85f5a89ff8aa76
Jaca777 <szymonrodant@gmail.com>
2016-06-30 14:16:06 +0200

    Renderer exception

---
+++ graphics/src/main/java/pl/warp/engine/graphics/RendererException.java
@@ -0,0 +1,11 @@
+package pl.warp.engine.graphics;
+
+/**
+ * @author Jaca777
+ *         Created 2016-06-30 at 13
+ */
+public class RendererException extends RuntimeException {
+    public RendererException(String message) {
+        super(message);
+    }
+}
