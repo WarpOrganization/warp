3bad77c4334a204135759891008ec38fbb3143f5
Jaca777 <szymonrodant@gmail.com>
2016-08-15 20:56:46 +0200

    Resource management module init.

---
+++ resourcemanagement/src/main/java/pl/warp/engine/resourcemanagement/ResourceManagementTask.java
@@ -0,0 +1,27 @@
+package pl.warp.engine.resourcemanagement;
+
+import pl.warp.engine.core.EngineTask;
+
+/**
+ * @author Jaca777
+ *         Created 2016-08-15 at 20
+ */
+public class ResourceManagementTask extends EngineTask {
+    @Override
+    protected void onInit() {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void onClose() {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void update(int delta) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+}
