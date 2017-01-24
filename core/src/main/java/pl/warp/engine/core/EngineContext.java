ea8ed476e935ac872b9dcac55b72f1e4364be7e7
Jaca777 <szymonrodant@gmail.com>
2017-01-24 18:28:36 +0100

    Jar location context

--- core/src/main/java/pl/warp/engine/core/EngineContext.java
+++ core/src/main/java/pl/warp/engine/core/EngineContext.java
@@ -1,13 +1,30 @@
+
 package pl.warp.engine.core;
 
 import pl.warp.engine.core.scene.Scene;
 import pl.warp.engine.core.scene.script.ScriptContext;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 /**
  * @author Jaca777
  *         Created 2016-12-04 at 14
  */
 public class EngineContext {
+
+    public static final String JAR_LOCATION = getJarLocation();
+
+    private static String getJarLocation() {
+        try {
+            String path = EngineContext.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            return URLDecoder.decode(path, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     private Scene scene;
     private ScriptContext scriptContext;
 
@@ -26,4 +43,5 @@ public class EngineContext {
     public void setScriptContext(ScriptContext scriptContext) {
         this.scriptContext = scriptContext;
     }
+
 }
