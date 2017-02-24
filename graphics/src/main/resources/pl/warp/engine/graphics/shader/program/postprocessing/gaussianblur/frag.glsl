2c005c5668ded9d92cd9efde839174a852986f5f
Jaca777 <szymonrodant@gmail.com>
2017-02-24 17:08:45 +0100

    even better bloom

--- graphics/src/main/resources/pl/warp/engine/graphics/shader/program/postprocessing/gaussianblur/frag.glsl
+++ graphics/src/main/resources/pl/warp/engine/graphics/shader/program/postprocessing/gaussianblur/frag.glsl
@@ -14,7 +14,7 @@ const float[8] weights = float[](0.034619, 0.044859, 0.055857, 0.066833, 0.07684
 void main(void){
 	fragColor = vec4(0.0);
     for(int i = 0; i < 15; i++) {
-        vec2 coord = (texCoord + blurDirection * pixelSize * (i - 7)) * 2;
+        vec2 coord = (texCoord + blurDirection * pixelSize * (i - 7)) * 3;
         coord.x = clamp(coord.x, 0.001, 0.999);
         coord.y = clamp(coord.y, 0.001, 0.999);
         fragColor += texture(tex, coord) * weights[abs(i - 7)];
