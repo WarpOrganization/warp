e6220011073d9d7484ea9e99d74671d6277ad0e5
Jaca777 <szymonrodant@gmail.com>
2017-03-25 00:36:52 +0100

    planet (xd)

--- game/src/main/resources/pl/warp/game/graphics/effects/planet/generator/surface/frag.glsl
+++ game/src/main/resources/pl/warp/game/graphics/effects/planet/generator/surface/frag.glsl
@@ -1,7 +1,12 @@
 #version 330
 
+#include "util/noise3d"
+
 smooth in vec3 spherePos;
 
 void main() {
-	gl_FragColor = vec4(spherePos, 1);
+	float n = snoise(spherePos * 2);
+	if(n > 0.2)
+	    gl_FragColor = vec4(0, 1, 0, 1);
+	else gl_FragColor = vec4(0, 0, 1, 0);
 }
