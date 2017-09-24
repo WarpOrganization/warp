edd66dc0f801fcf01a6b520cc08aa28eaf14aff7
Hubertus248 <hubertus248@gmail.com>
2017-09-24 19:49:51 +0200

    ray tests in progress

---
+++ physics/src/main/java/pl/warp/engine/physics/RayTester.java
@@ -0,0 +1,105 @@
+package pl.warp.engine.physics;
+
+
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.bullet.collision.ClosestRayResultCallback;
+import org.joml.Vector3f;
+import pl.warp.engine.core.component.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Hubertus
+ *         Created 01.08.16
+ */
+public class RayTester {
+
+    private ClosestRayResultCallback result;
+    private ClosestRayResultCallback result2; //used only by physics thread
+
+    private PhysicsWorld world;
+
+    private Map<String, RayTestTask> taskMap;
+
+    public void init(PhysicsWorld world){
+        this.world = world;
+        taskMap = new HashMap<>();
+        result = new ClosestRayResultCallback(new Vector3(), new Vector3());
+        result2 = new ClosestRayResultCallback(new Vector3(), new Vector3());
+    }
+
+//    public void update() {
+//        taskMap.forEach((s, rayTestTask) -> {
+//            clearResult(result2);
+//            result2.setRayFromWorld(rayTestTask.getStartPos());
+//            result2.setRayToWorld(rayTestTask.getEndPos());
+//            synchronized (world) {
+//                world.getDynamicsWorld().rayTest(rayTestTask.getStartPos(), rayTestTask.getEndPos(), result2);
+//            }
+//            if (result2.hasHit()) {
+//                rayTestTask.setHasHit(true);
+//                Component hit;
+//                synchronized (world) {
+//                    hit = world.getComponent(result2.getCollisionObject().getUserValue());
+//                }
+//                rayTestTask.setHit(hit);
+//            } else {
+//                rayTestTask.setHasHit(false);
+//                rayTestTask.setHit(null);
+//            }
+//        });
+//    }
+//
+//    public Component rayTest(Vector3 startPos, Vector3 endPos) {
+//        clearResult(result);
+//        result.setRayFromWorld(startPos);
+//        result.setRayToWorld(endPos);
+//        synchronized (world) {
+//            world.getCollisionWorld().rayTest(startPos, endPos, result);
+//        }
+//        if (result.hasHit()) {
+//            Component component;
+//            synchronized (world) {
+//                component = world.getComponent(result.getCollisionObject().getUserValue());
+//            }
+//            return component;
+//        } else {
+//            return null;
+//        }
+//    }
+
+    public void addRayTestTask(String key, Vector3f startPos, Vector3f endPos) {
+        synchronized (taskMap) {
+            taskMap.put(key, new RayTestTask(startPos, endPos));
+        }
+    }
+
+    public void removeRayTestTask(String key) {
+        synchronized (taskMap) {
+            taskMap.remove(key);
+        }
+    }
+
+    public Component getComponentHit(String key) {
+        return taskMap.get(key).getHit();
+    }
+
+    public RayTestTask getTask(String key){
+        return taskMap.get(key);
+    }
+
+    private Vector3 startPos = new Vector3();
+    private Vector3 endPos = new Vector3();
+
+//    public synchronized Component rayTest(Vector3f startPos, Vector3f endPos) {
+//        this.startPos.set(startPos.x, startPos.y, startPos.z);
+//        this.endPos.set(endPos.x, endPos.y, endPos.z);
+//        return rayTest(this.startPos, this.endPos);
+//    }
+
+    private void clearResult(ClosestRayResultCallback result) {
+        result.setCollisionObject(null);
+        result.setClosestHitFraction(1f);
+    }
+}
