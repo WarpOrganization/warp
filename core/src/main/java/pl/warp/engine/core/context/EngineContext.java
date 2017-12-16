519c8129c9c82909993abb76f0fb257c2de81899
Jaca777 <szymonrodant@gmail.com>
2017-12-16 19:09:19 +0100

    sth

--- core/src/main/java/pl/warp/engine/core/context/EngineContext.java
+++ core/src/main/java/pl/warp/engine/core/context/EngineContext.java
@@ -60,7 +60,6 @@ public class EngineContext {
         return scriptManager;
     }
 
-
     public EventDispatcher getEventDispatcher() {
         return eventDispatcher;
     }
--- resourcemanagement/src/main/java/pl/warp/engine/resourcemanagement/scene/ChunkUnloader.java
+++
@@ -1,9 +0,0 @@
-package pl.warp.engine.resourcemanagement.scene;
-
-/**
- * @author Jaca777
- *         Created 2016-08-15 at 21
- */
-public interface ChunkUnloader {
-    void unloadChunk(Chunk chunk);
-}
