9b97970d623168564b31e6902d1f3df58cb56826
Jaca777 <szymonrodant@gmail.com>
2018-01-03 22:01:05 +0100

    even less uints

--- graphics/src/main/resources/pl/warp/engine/graphics/program/util/flags.glsl
+++ graphics/src/main/resources/pl/warp/engine/graphics/program/util/flags.glsl
@@ -2,6 +2,6 @@ bool isSet(uint flags, int index) {
     return ((flags >> index) & 0x01) == 1;
 }
 
-uint flag(bool flag, int index){
+int flag(bool flag, int index){
     return (flag ? 1 : 0) << index;
 }
