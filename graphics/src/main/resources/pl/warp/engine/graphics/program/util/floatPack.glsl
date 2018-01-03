1acf17ef00d805a1ffdfac7891b4ac2d3ccef480
Jaca777 <szymonrodant@gmail.com>
2018-01-03 21:44:40 +0100

    fixed shifts

--- graphics/src/main/resources/pl/warp/engine/graphics/program/util/floatPack.glsl
+++ graphics/src/main/resources/pl/warp/engine/graphics/program/util/floatPack.glsl
@@ -1,33 +1,33 @@
 uint nBitMask(int n) {
-    return ((0x01 << uint(n)) - 1);
+    return ((0x01 << n) - 1);
 }
 
 uint nBitMask(uint n) {
-    return ((0x01 << uint(n)) - 1);
+    return ((0x01 << int(n)) - 1);
 }
 
 uint packUnsignedNorm(float f, int bits) {
     uint ubits = uint(bits);
-    return uint(round(clamp(f, 0.0, 1.0) * (0x01 << (ubits - 1)))) & nBitMask(ubits);
+    return uint(round(clamp(f, 0.0, 1.0) * (0x01 << (bits - 1)))) & nBitMask(ubits);
 }
 
 
 float unpackUnsignedNorm(uint i, int bits) {
     uint ubits = uint(bits);
-    return clamp((i & nBitMask(ubits)) / ((0x01 << (ubits - 1)) * 1.0), 0.0, 1.0);
+    return clamp((i & nBitMask(ubits)) / ((0x01 << (bits - 1)) * 1.0), 0.0, 1.0);
 }
 
 uint packSignedNorm(float f, int bits) {
     uint ubits = uint(bits);
     uint sign = (f < 0) ? 0 : 1;
-    uint i = uint(round(clamp(f, -1.0, 1.0) * (0x01 << (ubits - 2)))) & nBitMask(ubits - 1);
-    return ((sign == 0) ? nBitMask(ubits - 1) ^ i : i) | (sign << (ubits - 1));
+    uint i = uint(round(clamp(f, -1.0, 1.0) * (0x01 << (bits - 2)))) & nBitMask(ubits - 1);
+    return ((sign == 0) ? nBitMask(ubits - 1) ^ i : i) | (sign << (bits - 1));
 }
 
 float unpackSignedNorm(uint i, int bits) {
     uint ubits = uint(bits);
-    float f = clamp((i & nBitMask(ubits - 1)) / ((0x01 << (ubits - 2)) * 1.0), -1.0, 1.0);
-    uint sign = (i >> (ubits - 1)) & 0x01;
+    float f = clamp((i & nBitMask(ubits - 1)) / ((0x01 << (bits - 2)) * 1.0), -1.0, 1.0);
+    uint sign = (i >> (bits - 1)) & 0x01;
     return f * (sign * 2 - 1);
 }
 
@@ -41,12 +41,12 @@ uint v2PackSignedNorm(vec2 v, int bits) {
 
 vec2 v2UnpackUnsignedNorm(uint i, int bits) {
     float y = unpackUnsignedNorm(i, bits);
-    float x = unpackUnsignedNorm(i >> uint(bits), bits);
+    float x = unpackUnsignedNorm(i >> bits, bits);
     return vec2(x,y);
 }
 
 vec2 v2UnpackSignedNorm(uint i, int bits) {
     float y = unpackSignedNorm(i, bits);
-    float x = unpackSignedNorm(i >> uint(bits), bits);
+    float x = unpackSignedNorm(i >> bits, bits);
     return vec2(x,y);
 }
