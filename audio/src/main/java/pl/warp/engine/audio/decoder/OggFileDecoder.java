40c3b4959b0b1fb652ad1121fd5dc19e8c4d946b
MarconZet <marcin.zlakowski@gmail.com>
2018-01-03 19:49:32 +0100

    Memory management revamp.

--- audio/src/main/java/pl/warp/engine/audio/decoder/OggFileDecoder.java
+++ audio/src/main/java/pl/warp/engine/audio/decoder/OggFileDecoder.java
@@ -1,17 +1,15 @@
 package pl.warp.engine.audio.decoder;
 
-import org.lwjgl.BufferUtils;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.MemoryStack;
 
-import java.io.File;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import java.nio.ShortBuffer;
-import java.util.Arrays;
 
 import static org.lwjgl.stb.STBVorbis.stb_vorbis_decode_filename;
-import static org.lwjgl.system.MemoryStack.stackMallocInt;
-import static org.lwjgl.system.MemoryStack.stackPop;
-import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+import static org.lwjgl.system.MemoryUtil.memByteBuffer;
 
 /**
  * Created by MarconZet on 30.04.2017.
@@ -19,20 +17,22 @@ import static org.lwjgl.system.MemoryStack.stackPush;
 public class OggFileDecoder{
 
     public static SoundData decode(String filename) {
-        stackPush();
-        IntBuffer channelsBuffer = stackMallocInt(1);
-        stackPush();
-        IntBuffer sampleRateBuffer = stackMallocInt(1);
 
-        ShortBuffer rawAudioBuffer = stb_vorbis_decode_filename(filename, channelsBuffer, sampleRateBuffer);
+        try(MemoryStack stack = stackPush()) {
+            IntBuffer channelsBuffer = stackMallocInt(1);
+            stackPush();
+            IntBuffer sampleRateBuffer = stackMallocInt(1);
+            PointerBuffer rawAudioBuffer = stack.pointers(NULL);
 
-        int channels = channelsBuffer.get();
-        int sampleRate = sampleRateBuffer.get();
-        stackPop();
-        stackPop();
-        short[] s = rawAudioBuffer.array();
-        ByteBuffer bb = ByteBuffer.allocate(s.length * 2);
-        bb.asShortBuffer().put(s);
-        return new SoundData(bb, sampleRate, channels, 16);
+            int result = stb_vorbis_decode_filename(filename, channelsBuffer, sampleRateBuffer, rawAudioBuffer);
+            int channels = channelsBuffer.get();
+            int sampleRate = sampleRateBuffer.get();
+            ByteBuffer rawAudio = memByteBuffer(rawAudioBuffer.get(0), result * channels);
+
+            stackPop();
+            stackPop();
+
+            return new SoundData(rawAudio, sampleRate, channels, 16);
+        }
     }
 }
