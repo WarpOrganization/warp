d24450716920df4fa36a4e364cbdeba7b4b0ea6a
KocproZ <kocproz@protonmail.com>
2018-07-14 14:11:55 +0200

    repaired 4 jenkins

---
+++ gamecontent/src/main/java/net/warpgame/content/ConsoleInputEvent.java
@@ -0,0 +1,48 @@
+package net.warpgame.content;
+
+import net.warpgame.engine.core.context.service.Service;
+import net.warpgame.engine.core.serialization.Deserializer;
+import net.warpgame.engine.core.serialization.Serialization;
+import net.warpgame.engine.core.serialization.Serializer;
+import net.warpgame.engine.net.event.NetworkEvent;
+
+/**
+ * @author KocproZ
+ * Created 2018-06-12 at 09:23
+ */
+public class ConsoleInputEvent extends NetworkEvent {
+
+    private String input;
+
+    public ConsoleInputEvent(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    @Service
+    public static class ConsoleInputSerializer extends Serialization<ConsoleInputEvent> {
+
+        public ConsoleInputSerializer() {
+            super(ConsoleInputEvent.class);
+        }
+
+        @Override
+        public void serialize(ConsoleInputEvent object, Serializer serializer) {
+            serializer.write(object.getInput());
+        }
+
+        @Override
+        public ConsoleInputEvent deserialize(Deserializer deserializer) {
+            return new ConsoleInputEvent(deserializer.getString());
+        }
+
+        @Override
+        public int getType() {
+            return 8;
+        }
+    }
+
+}
