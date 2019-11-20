2a880f88cbc7d93e702d815f8af83c54af68b056
MarconZet <marcin.zlakowski@gmail.com>
2019-11-20 01:06:33 +0100

    Debugging

--- graphics/src/main/java/net/warpgame/engine/graphics/memory/scene/ubo/VulkanTransform.java
+++ graphics/src/main/java/net/warpgame/engine/graphics/memory/scene/ubo/VulkanTransform.java
@@ -7,13 +7,7 @@ import net.warpgame.engine.graphics.memory.Buffer;
 import net.warpgame.engine.graphics.memory.scene.Loadable;
 import net.warpgame.engine.graphics.window.SwapChain;
 import org.joml.Matrix4fc;
-import org.lwjgl.BufferUtils;
-import org.lwjgl.PointerBuffer;
 
-import java.io.FileNotFoundException;
-
-import static org.lwjgl.system.MemoryUtil.memAddress;
-import static org.lwjgl.system.MemoryUtil.memCopy;
 import static org.lwjgl.util.vma.Vma.VMA_MEMORY_USAGE_CPU_TO_GPU;
 import static org.lwjgl.vulkan.VK10.*;
 
@@ -31,7 +25,7 @@ public class VulkanTransform extends Loadable {
     }
 
     @Override
-    public void load(Device device, Allocator allocator, CommandPool commandPool) throws FileNotFoundException {
+    public void load(Device device, Allocator allocator, CommandPool commandPool) {
         this.device = device;
 
         long bufferSize = UniformBufferObject.sizeOf();
@@ -51,11 +45,7 @@ public class VulkanTransform extends Loadable {
 
     public void update(Matrix4fc modelMatrix, Matrix4fc viewMatrix, Matrix4fc projectionMatrix, int currentImage){
         UniformBufferObject ubo = new UniformBufferObject(modelMatrix, viewMatrix, projectionMatrix);
-        PointerBuffer pData = BufferUtils.createPointerBuffer(1);
-        vkMapMemory(device.get(), uniformBuffers[currentImage].get(), 0, UniformBufferObject.sizeOf(), 0, pData);
-        long data = pData.get(0);
-        memCopy(memAddress(ubo.data), data, UniformBufferObject.sizeOf());
-        vkUnmapMemory(device.get(),  uniformBuffers[currentImage].get());
+        Buffer.copyBuffer(ubo.data, uniformBuffers[currentImage], UniformBufferObject.sizeOf());
     }
 
     @Override
